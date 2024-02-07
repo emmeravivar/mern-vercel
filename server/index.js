0de948a334818ee90265c31b9e4b7566910d9113
emmeravivar <em.meravivar@gmail.com>
2024-02-07 20:20:02 +0100

    mongoose

--- server/index.js
+++ server/index.js
@@ -1,22 +1,39 @@
-    const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-    const app = express()
+//Conectar nuestra BBDD
+const connectBD = async () => {
+    try {
+        const connection = await mongoose.connect('mongodb+srv://root:root@cluster0.i04c4yl.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const url = `${connection.connection.host}:${connection.connection.port}`;
+        console.log(`MongoDB conecta en: ${url}`);
+    } catch (error) {
+        console.log(`error`);
+        process.exit(1); // normalmente node acaba los procesos con 0.Forzar proceso termine
+    }
+};
+//Conectando nuestra BBDD
+connectBD();
 
-    app.get('/', (req, res) => {
-        res.send('Express JS on Vercel or not')
-    })
+app.get('/', (req, res) => {
+    res.send('Express JS on Vercel or not');
+});
 
-    app.get('/ping', (req, res) => {
-        res.send('pong')
-    })
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
 
-    const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-    app.listen(port, (err, res) => {
-        if (err) {
-            console.log(err)
-            return res.status(500).send(err.message)
-        } else {
-            console.log('[INFO] Server Running on port:', port)
-        }
-    })
+app.listen(port, (err, res) => {
+    if (err) {
+        console.log(err);
+        return res.status(500).send(err.message);
+    } else {
+        console.log('[INFO] Server Running on port:', port);
+    }
+});
